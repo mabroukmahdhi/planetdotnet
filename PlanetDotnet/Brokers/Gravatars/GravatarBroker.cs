a854d42344bf95a5a2bf23526f300652b5fbb5a8
Mabrouk Mahdhi <mabrouk@mahdhi.com>
2022-07-27 18:47:00 +0200

    BROKERS: Get Gravatar Image

---
+++ PlanetDotnet/Brokers/Gravatars/GravatarBroker.cs
@@ -0,0 +1,53 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) .NET Community, Mabrouk Mahdhi
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Microsoft.Extensions.Configuration;
+using PlanetDotnet.Models.Foundations.Abstractions;
+using PlanetDotnet.Models.Foundations.Configurations;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PlanetDotnet.Brokers.Gravatars
+{
+    public class GravatarBroker : IGravatarBroker
+    {
+        private readonly LocalConfigurations localConfigurations;
+
+        public GravatarBroker(IConfiguration configuration) =>
+            this.localConfigurations =
+                configuration.Get<LocalConfigurations>();
+
+        public string GetGravatarImage(IAmACommunityMember member)
+        {
+            int size = this.localConfigurations.DefaultGravatarImageSize;
+
+            var hash = member.GravatarHash;
+
+            if (string.IsNullOrWhiteSpace(hash))
+            {
+                hash = member.EmailAddress.Trim().ToLowerInvariant();
+
+                hash = ToMd5Hash(hash).ToLowerInvariant();
+            }
+
+            var defaultImage = this.localConfigurations.DefaultGravatarImage;
+
+            return $"//www.gravatar.com/avatar/{hash}.jpg?s={size}&d={defaultImage}";
+        }
+
+        private static string ToMd5Hash(string toHash)
+        {
+            var unhashedBytes = Encoding.UTF8.GetBytes(toHash);
+            var hashedBytes = MD5.Create().ComputeHash(unhashedBytes);
+
+            var hashedString = string.Join(string.Empty,
+                hashedBytes.Select(b => b.ToString("X2")).ToArray());
+            return hashedString;
+        }
+    }
+
+}
---
+++ PlanetDotnet/Models/Foundations/Configurations/LocalConfigurations.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) .NET Community, Mabrouk Mahdhi
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+namespace PlanetDotnet.Models.Foundations.Configurations
+{
+    public class LocalConfigurations
+    {
+        public string DefaultGravatarImage { get; set; }
+        public int DefaultGravatarImageSize { get; set; } 
+    }
+}
