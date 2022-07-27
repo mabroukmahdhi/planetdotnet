5808f8f2d16058406f0649c3b94a8dec27699ca4
Mabrouk Mahdhi <mabrouk@mahdhi.com>
2022-07-27 19:14:19 +0200

    VIEWS: Adding HeaderBase

---
+++ PlanetDotnet/Views/Bases/Headers/HeaderBase.razor.cs
@@ -0,0 +1,39 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) .NET Community, Mabrouk Mahdhi
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+using PlanetDotnet.Models.Views.Bases.Headers;
+using SharpStyles.Models;
+
+namespace PlanetDotnet.Views.Bases.Headers
+{
+    public partial class HeaderBase : ComponentBase
+    {
+        [Parameter]
+        public RenderFragment Left { get; set; }
+
+        [Parameter]
+        public RenderFragment Right { get; set; }
+
+        public SharpStyle Styles { get; set; }
+        protected override void OnInitialized()
+        {
+            SetupStyles();
+        }
+        private void SetupStyles()
+        {
+            Styles = new HeaderStyles
+            {
+                NavbarCollapsed = new SharpStyle
+                {
+                    Overflow = "hidden",
+                    MaxHeight = "none !important",
+                    Height = "auto !important"
+                }
+            };
+        }
+    }
+}
---
+++ PlanetDotnet/Views/Bases/Styles/StyleBase.razor.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) .NET Community, Mabrouk Mahdhi
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+using SharpStyles.Models;
+
+namespace PlanetDotnet.Views.Bases.Styles
+{
+    public partial class StyleBase : ComponentBase
+    {
+        [Parameter]
+        public SharpStyle Style { get; set; }
+    }
+}
