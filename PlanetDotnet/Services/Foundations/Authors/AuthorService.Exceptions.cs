9d914443cc9b72c534bfa12dfc50e31ac8dba03b
Mabrouk Mahdhi <mabrouk@mahdhi.com>
2022-07-27 18:57:07 +0200

    FOUNDATIONS: Retrieve All Authors

---
+++ PlanetDotnet/Services/Foundations/Authors/AuthorService.Exceptions.cs
@@ -0,0 +1,37 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) .NET Community, Mabrouk Mahdhi
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using PlanetDotnet.Services.Foundations.Authors.Exceptions;
+using System;
+
+namespace PlanetDotnet.Services.Foundations.Authors
+{
+    public partial class AuthorService
+    {
+        private delegate string ReturnStringFunction();
+
+        private string TryCatch(ReturnStringFunction returnStringFunction)
+        {
+            try
+            {
+                return returnStringFunction();
+            }
+            catch (NullAuthorException nullAuthorException)
+            {
+                throw CreateAndLogValidationException(nullAuthorException);
+            }
+        }
+
+        private AuthorValidationException CreateAndLogValidationException(Exception exception)
+        {
+            var authorValidationException = new AuthorValidationException(exception);
+            this.loggingBroker.LogError(authorValidationException);
+
+            return authorValidationException;
+        }
+    }
+
+}
---
+++ PlanetDotnet/Services/Foundations/Authors/AuthorService.cs
@@ -0,0 +1,45 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) .NET Community, Mabrouk Mahdhi
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using PlanetDotnet.Brokers.Authors;
+using PlanetDotnet.Brokers.Gravatars;
+using PlanetDotnet.Brokers.Loggings;
+using PlanetDotnet.Models.Foundations.Abstractions;
+using System.Collections.Generic;
+
+namespace PlanetDotnet.Services.Foundations.Authors
+{
+    public partial class AuthorService : IAuthorService
+    {
+        private readonly IAuthorBroker authorBroker;
+        private readonly IGravatarBroker gravatarBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public AuthorService(
+            IAuthorBroker authorBroker,
+            IGravatarBroker gravatarBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.authorBroker = authorBroker;
+            this.gravatarBroker = gravatarBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public IEnumerable<IAmACommunityMember> RetrieveAllAuthers()
+        {
+            return this.authorBroker.SelectAllAuthers();
+        }
+
+        public string RetrieveAuthorImage(IAmACommunityMember author) =>
+        TryCatch(() =>
+        {
+            ValidateAuthor(author);
+
+            return this.gravatarBroker.GetGravatarImage(author);
+        });
+    }
+
+}
