7638c71fe7883f4ddbe93b3ca566f79f7159417f
Mabrouk Mahdhi <Mabrouk.Mahdhi@messer-is.com>
2022-07-28 11:20:16 +0200

    VIEWS: Adding Welcome View

---
+++ PlanetDotnet/Models/Views/Welcomes/WelcomeView.cs
@@ -0,0 +1,23 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) .NET Community, Mabrouk Mahdhi
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using PlanetDotnet.Models.Foundations.Abstractions;
+using System.Collections.Generic;
+
+namespace PlanetDotnet.Models.Views.Welcomes
+{
+    public class WelcomeView
+    {
+        public string TwitterLink { get; set; }
+        public string FacebookLink { get; set; }
+        public string FeedLink { get; set; }
+        public string PreviewPagePath { get; set; }
+        public string AuthorsPagePath { get; set; }
+        public string GithubRepository { get; set; }
+        public string BadgeImagePath { get; set; }
+        public IEnumerable<IAmACommunityMember> Podcasts { get; set; }
+    }
+}
---
+++ PlanetDotnet/Views/Components/WelcomeComponents/WelcomeComponent.razor.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) .NET Community, Mabrouk Mahdhi
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+
+namespace PlanetDotnet.Views.Components.WelcomeComponents
+{
+    public partial class WelcomeComponent : ComponentBase
+    {
+    }
+}
