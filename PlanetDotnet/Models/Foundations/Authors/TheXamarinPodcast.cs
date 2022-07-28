2305543c33b1ea20aa8effe920209eafbe6224c2
Mabrouk Mahdhi <Mabrouk.Mahdhi@messer-is.com>
2022-07-28 13:25:29 +0200

    COMPONENTS: Localize Podcast Components

---
+++ PlanetDotnet/Models/Foundations/Authors/TheXamarinPodcast.cs
@@ -0,0 +1,38 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) .NET Community, Mabrouk Mahdhi
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using PlanetDotnet.Models.Foundations.Abstractions;
+using PlanetDotnet.Models.Foundations.GeoPositions;
+using System.Collections.Generic;
+using System;
+using PlanetDotnet.Models.Foundations.Tags;
+
+namespace PlanetDotnet.Models.Foundations.Authors
+{
+    public class TheXamarinPodcast : IAmACommunityMember, IAmAPodcast
+    {
+        public string FirstName => "The .NET MAUI";
+        public string LastName => "Podcast";
+        public string StateOrRegion => "Internet";
+        public string EmailAddress => "hello@xamarin.com";
+        public string ShortBioOrTagLine => "is the official podcast of .NET MAUI!";
+        public Uri WebSite => new Uri("http://www.xamarinpodcast.com");
+
+        public IEnumerable<Uri> FeedUris
+        {
+            get { yield return new Uri("https://feeds.fireside.fm/xamarinpodcast/rss"); }
+        }
+
+        public string TwitterHandle => "XamarinPodcast";
+        public string GravatarHash => "e7d599acf639d9d3883a0fd477b3ba42";
+        public string GitHubHandle => string.Empty;
+        public GeoPosition Position => GeoPosition.Empty;
+        public string FeedLanguageCode => "en";
+
+        public IEnumerable<Tag> Tags => new List<Tag> { Tag.Xamarin, Tag.DotNetMAUI };
+    }
+
+}
---
+++ PlanetDotnet/Models/Foundations/Authors/TheXamarinShow.cs
@@ -0,0 +1,36 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) .NET Community, Mabrouk Mahdhi
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using PlanetDotnet.Models.Foundations.Abstractions;
+using PlanetDotnet.Models.Foundations.GeoPositions;
+using System.Collections.Generic;
+using System;
+using PlanetDotnet.Models.Foundations.Tags;
+
+namespace PlanetDotnet.Models.Foundations.Authors
+{
+    public class TheXamarinShow : IAmACommunityMember, IAmAPodcast
+    {
+        public string FirstName => "The Xamarin";
+        public string LastName => "Show";
+        public string StateOrRegion => "Channel 9";
+        public string EmailAddress => "";
+        public string ShortBioOrTagLine => "is a Weekly Developer Show for Xamarin Developers";
+        public Uri WebSite => new Uri("http://xamarinshow.com");
+
+        public IEnumerable<Uri> FeedUris
+        {
+            get { yield return new Uri("https://channel9.msdn.com/Shows/XamarinShow/feed"); }
+        }
+
+        public string TwitterHandle => "TheXamarinShow";
+        public string GravatarHash => "7a0c7da0279b4e90439e780fa01924e0";
+        public string GitHubHandle => string.Empty;
+        public GeoPosition Position => new GeoPosition(47.6451360, -122.1309390);
+        public string FeedLanguageCode => "en";
+        public IEnumerable<Tag> Tags => new List<Tag> { Tag.Xamarin };
+    }
+}
